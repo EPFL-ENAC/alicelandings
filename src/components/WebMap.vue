4baae960dbb1dbf183a8d2c1baa2b54094ef645a
David Tang <david.tang@epfl.ch>
2022-01-24 09:28:40 +0100

    ✨ mouse coordinate

--- src/components/WebMap.vue
+++ src/components/WebMap.vue
@@ -1,13 +1,15 @@
 <template>
   <v-responsive aspect-ratio="1">
-    <l-map :zoom="zoom" :center="center">
+    <l-map ref="lMap" :zoom="zoom" :center="center">
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
     </l-map>
   </v-responsive>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import L, { LeafletMouseEvent, Map } from "leaflet";
+import "vue-class-component/hooks";
+import { Component, Ref, Vue } from "vue-property-decorator";
 import { LMap, LTileLayer } from "vue2-leaflet";
 
 @Component({
@@ -22,5 +24,26 @@ export default class WebMap extends Vue {
     '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors';
   readonly zoom = 15;
   readonly center = [46.51725, 6.56885];
+
+  @Ref()
+  readonly lMap!: LMap;
+
+  mounted(): void {
+    const Coordinates = L.Control.extend({
+      onAdd: (map: Map) => {
+        const container = L.DomUtil.create("div");
+        map.addEventListener("mousemove", (e: LeafletMouseEvent) => {
+          container.innerHTML = `
+          Latitude/Longitude: 
+          (${e.latlng.lat.toFixed(4)}; ${e.latlng.lng.toFixed(4)})`;
+        });
+        map.addEventListener("mouseout", () => {
+          container.innerHTML = "";
+        });
+        return container;
+      },
+    });
+    this.lMap.mapObject.addControl(new Coordinates({ position: "bottomleft" }));
+  }
 }
 </script>
