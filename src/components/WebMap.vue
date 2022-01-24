4acabb345f8a88bcffd593798ceb438109589770
David Tang <david.tang@epfl.ch>
2022-01-24 14:04:19 +0100

    ✨ select base layer

--- src/components/WebMap.vue
+++ src/components/WebMap.vue
@@ -1,12 +1,24 @@
 <template>
-  <v-responsive aspect-ratio="1">
-    <l-map ref="lMap" :zoom="zoom" :center="center">
-      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    </l-map>
-  </v-responsive>
+  <v-row>
+    <v-col cols="12">
+      <v-select
+        v-model="baseLayer"
+        :items="baseLayerItems"
+        label="Base Layer"
+      ></v-select>
+    </v-col>
+    <v-col cols="12">
+      <v-responsive aspect-ratio="1.6">
+        <l-map ref="lMap" :zoom="zoom" :center="center">
+          <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+        </l-map>
+      </v-responsive>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
+import { SelectItemObject } from "@/utils/vuetify";
 import L, { LeafletMouseEvent, Map } from "leaflet";
 import "leaflet.bigimage/dist/Leaflet.BigImage.min.js";
 import "vue-class-component/hooks";
@@ -20,14 +32,44 @@ import { LMap, LTileLayer } from "vue2-leaflet";
   },
 })
 export default class WebMap extends Vue {
-  readonly url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-  readonly attribution =
-    '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors';
   readonly zoom = 15;
   readonly center = [46.51725, 6.56885];
 
+  readonly baseLayerItems: SelectItemObject<string, BaseLayer>[] = [
+    {
+      text: "OpenStreetMap",
+      value: {
+        url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        attribution:
+          '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      },
+    },
+    {
+      text: "swisstopo",
+      value: {
+        url: "https://wmts20.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg",
+        attribution:
+          '&copy; <a target="_blank" href="https://www.swisstopo.admin.ch/en/home.html">swisstopo</a>',
+      },
+    },
+  ];
+
   @Ref()
   readonly lMap!: LMap;
+
+  baseLayer: BaseLayer = this.baseLayerItems[0].value;
+
+  get map(): Map {
+    return this.lMap.mapObject;
+  }
+
+  get url(): string {
+    return this.baseLayer.url;
+  }
+
+  get attribution(): string | undefined {
+    return this.baseLayer.attribution;
+  }
 
   mounted(): void {
     const Coordinates = L.Control.extend({
@@ -44,10 +86,21 @@ export default class WebMap extends Vue {
         return container;
       },
     });
-    this.lMap.mapObject.addControl(new Coordinates({ position: "bottomleft" }));
+    this.map.addControl(new Coordinates({ position: "bottomleft" }));
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (L.control as any).BigImage().addTo(this.lMap.mapObject);
+    (L.control as any).BigImage().addTo(this.map);
   }
 }
+
+interface BaseLayer {
+  url: string;
+  attribution?: string;
+}
 </script>
+
+<style scoped>
+.leaflet-container {
+  z-index: 0;
+}
+</style>
