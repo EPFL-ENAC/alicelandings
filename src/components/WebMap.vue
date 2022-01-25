0075b6bf92dad12421130fbe6468e447d63ab667
David Tang <david.tang@epfl.ch>
2022-01-25 09:40:16 +0100

    ✨ leaflet control layer

--- src/components/WebMap.vue
+++ src/components/WebMap.vue
@@ -1,14 +1,6 @@
 <template>
   <v-row>
     <v-col cols="6">
-      <v-select
-        v-model="baseLayer"
-        hide-details
-        :items="baseLayerItems"
-        label="Base Layer"
-      ></v-select>
-    </v-col>
-    <v-col cols="6">
       <v-file-input
         accept=".geojson, application/json"
         chips
@@ -36,16 +28,26 @@
       <v-progress-linear :active="loading" indeterminate></v-progress-linear>
       <v-responsive aspect-ratio="1.6">
         <l-map ref="lMap" :zoom="zoom" :center="center">
-          <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+          <l-control-layers position="topright"></l-control-layers>
+          <l-tile-layer
+            v-for="tileProvider in tileProviders"
+            :key="tileProvider.name"
+            :name="tileProvider.name"
+            :visible="tileProvider.visible"
+            :url="tileProvider.url"
+            :attribution="tileProvider.attribution"
+            layer-type="base"
+          />
+          <l-control-scale
+            position="bottomright"
+            :imperial="false"
+          ></l-control-scale>
+          <l-control-zoom position="bottomright"></l-control-zoom>
           <l-geo-json
             v-for="(item, index) in geoJsons"
             :key="index"
             :geojson="item"
           ></l-geo-json>
-          <l-control-scale
-            position="bottomright"
-            :imperial="false"
-          ></l-control-scale>
         </l-map>
       </v-responsive>
     </v-col>
@@ -53,18 +55,26 @@
 </template>
 
 <script lang="ts">
-import { SelectItemObject } from "@/utils/vuetify";
 import parseGeoRaster from "georaster";
 import GeoRasterLayer from "georaster-layer-for-leaflet";
 import L, { LeafletMouseEvent, Map } from "leaflet";
 import "leaflet.bigimage/dist/Leaflet.BigImage.min.js";
 import "vue-class-component/hooks";
 import { Component, Ref, Vue } from "vue-property-decorator";
-import { LControlScale, LGeoJson, LMap, LTileLayer } from "vue2-leaflet";
+import {
+  LControlLayers,
+  LControlScale,
+  LControlZoom,
+  LGeoJson,
+  LMap,
+  LTileLayer,
+} from "vue2-leaflet";
 
 @Component({
   components: {
+    LControlLayers,
     LControlScale,
+    LControlZoom,
     LGeoJson,
     LMap,
     LTileLayer,
@@ -73,23 +83,20 @@ import { LControlScale, LGeoJson, LMap, LTileLayer } from "vue2-leaflet";
 export default class WebMap extends Vue {
   readonly zoom = 15;
   readonly center = [46.51725, 6.56885];
-
-  readonly baseLayerItems: SelectItemObject<string, BaseLayer>[] = [
+  readonly tileProviders: TileProvider[] = [
     {
-      text: "OpenStreetMap",
-      value: {
-        url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-        attribution:
-          '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      },
+      name: "OpenStreetMap",
+      visible: true,
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     },
     {
-      text: "swisstopo",
-      value: {
-        url: "https://wmts20.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg",
-        attribution:
-          '&copy; <a target="_blank" href="https://www.swisstopo.admin.ch/en/home.html">swisstopo</a>',
-      },
+      name: "swisstopo",
+      visible: false,
+      attribution:
+        '&copy; <a target="_blank" href="https://www.swisstopo.admin.ch/en/home.html">swisstopo</a>',
+      url: "https://wmts20.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg",
     },
   ];
 
@@ -97,19 +104,31 @@ export default class WebMap extends Vue {
   readonly lMap!: LMap;
 
   loading = false;
-  baseLayer: BaseLayer = this.baseLayerItems[0].value;
   geoJsons: unknown[] = [];
 
   get map(): Map {
     return this.lMap.mapObject;
   }
 
-  get url(): string {
-    return this.baseLayer.url;
-  }
+  mounted(): void {
+    const Coordinates = L.Control.extend({
+      onAdd: (map: Map) => {
+        const container = L.DomUtil.create("div");
+        map.addEventListener("mousemove", (e: LeafletMouseEvent) => {
+          container.innerHTML = `
+          Latitude/Longitude:
+          (${e.latlng.lat.toFixed(4)}; ${e.latlng.lng.toFixed(4)})`;
+        });
+        map.addEventListener("mouseout", () => {
+          container.innerHTML = "";
+        });
+        return container;
+      },
+    });
+    this.map.addControl(new Coordinates({ position: "bottomleft" }));
 
-  get attribution(): string | undefined {
-    return this.baseLayer.attribution;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (L.control as any).BigImage().addTo(this.map);
   }
 
   addGeoJsonFiles(files: File[]): void {
@@ -138,32 +157,13 @@ export default class WebMap extends Vue {
         })
     );
   }
-
-  mounted(): void {
-    const Coordinates = L.Control.extend({
-      onAdd: (map: Map) => {
-        const container = L.DomUtil.create("div");
-        map.addEventListener("mousemove", (e: LeafletMouseEvent) => {
-          container.innerHTML = `
-          Latitude/Longitude:
-          (${e.latlng.lat.toFixed(4)}; ${e.latlng.lng.toFixed(4)})`;
-        });
-        map.addEventListener("mouseout", () => {
-          container.innerHTML = "";
-        });
-        return container;
-      },
-    });
-    this.map.addControl(new Coordinates({ position: "bottomleft" }));
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (L.control as any).BigImage().addTo(this.map);
-  }
 }
 
-interface BaseLayer {
+interface TileProvider {
+  name: string;
+  visible: boolean;
+  attribution: string;
   url: string;
-  attribution?: string;
 }
 </script>
 
