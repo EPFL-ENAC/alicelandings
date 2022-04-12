ef4a5dd117f52fcbdeaf43e6ecfb757e66bab104
David Tang <david.tang@epfl.ch>
2022-04-12 13:40:15 +0200

    ✨ hidden appbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app dense>
+    <v-app-bar v-if="appBar" app dense @mouseleave="appBar = false">
       <v-app-bar-title>Alice Landings</v-app-bar-title>
       <v-tabs>
         <v-tab to="/">Home</v-tab>
@@ -8,11 +8,22 @@
         <v-tab to="/about">About</v-tab>
       </v-tabs>
     </v-app-bar>
+    <v-app-bar v-if="!appBar" app height="8px" @mouseover="appBar = true">
+    </v-app-bar>
     <v-main>
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class App extends Vue {
+  appBar = false;
+}
+</script>
 
 <style lang="scss" scoped>
 .v-toolbar__title {
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,5 @@
 <template>
   <v-container>
     <h1>About</h1>
-    <p>
-      To address the challenges posed by climate change to our cities, it is
-      essential to acknowledge the role the environment plays in our everyday
-      lives. However our maps tend to silence our lived experience and how it
-      both shapes and is shaped by the places we move through. Consequently,
-      planning and design processes have often disregarded this and lost great
-      opportunities to collectively shape friendlier and human-oriented cities.
-      An analysis of active mobility practices in the commune of Vernier offers
-      us the chance to create alternative spatial representations to know this
-      lived dimension. By having maps able to address affective engagements,
-      social imaginaries, and collective memories, we can make our communities
-      matter more in the conversations about our cities' future.
-    </p>
   </v-container>
 </template>
