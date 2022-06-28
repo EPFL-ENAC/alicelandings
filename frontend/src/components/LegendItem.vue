b4d6d84e30a2dea519afcfe18fe925d9d76e8fa0
David Tang <david.tang@epfl.ch>
2022-06-28 08:48:22 +0200

    🍱 update legend icon

--- frontend/src/components/LegendItem.vue
+++ frontend/src/components/LegendItem.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="d-flex align-center">
     <v-img
-      class="flex-grow-0 ma-1"
+      class="flex-grow-0 my-1 mr-1"
       :src="imageSrc"
       contain
-      width="32"
-      height="32"
+      :width="size"
+      :height="size"
     ></v-img>
     <span class="ma-1">
       <slot>
@@ -25,12 +25,7 @@ export default class LegendItem extends Vue {
   readonly imageSrc!: string;
   @Prop({ type: String, default: "" })
   readonly text!: string;
+  @Prop({ type: Number, default: 42 })
+  readonly size!: number;
 }
 </script>
-
-<style lang="scss" scoped>
-.v-image {
-  border-style: solid;
-  border-width: 1px;
-}
-</style>
